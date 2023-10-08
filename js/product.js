da4d1741e5fece0135b6ba0e966be3168eae6507
geryarn <118037286+geryarn@users.noreply.github.com>
2023-10-08 18:26:44 +0700

    Add files via upload

--- js/product.js
+++ js/product.js
@@ -23,3 +23,42 @@ navLink.addEventListener("click", () => {
     }
 });
 
+
+// Fungsi untuk mengambil data menggunakan metode GET
+document.addEventListener('DOMContentLoaded', () => {
+  const productId = 1; // Ganti dengan ID produk yang diinginkan
+  const productDetailsContainer = document.getElementById('product-details');
+
+  fetch(`https://be-semarang-3.cyclic.app/api/catalog/${productId}`)
+      .then(response => {
+          if (response.ok) {
+              return response.json();
+          }
+          throw new Error('Network response was not ok.');
+      })
+      .then(productData => {
+          // Menampilkan data produk di halaman HTML
+          const { name, price, rebate, photo } = productData;
+          const productHTML = `
+              <div class="product-card">
+                  <div class="product-tumb">
+                      <img src="${photo}" alt="${name}">
+                  </div>
+                  <div class="product-details">
+                      <span class="product-catagory">Product Category</span>
+                      <h4><a href="#">${name}</a></h4>
+                      <p>${description}</p>
+                      <div class="product-bottom-details">
+                          <div class="product-price">Price: Rp ${price}</div>
+                          <div class="product-rebate">Rebate: ${rebate}%</div>
+                      </div>
+                  </div>
+              </div>
+          `;
+          productDetailsContainer.innerHTML = productHTML;
+      })
+      .catch(error => {
+          console.error('Error fetching product data:', error);
+      });
+});
+
